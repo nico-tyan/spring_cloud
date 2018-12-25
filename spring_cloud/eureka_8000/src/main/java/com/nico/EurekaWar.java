f78d5c5024890420ba2090c4f7ebcc03d42cfeb2
fangshu <nico@nico.com>
2018-12-25 15:31:46 +0800

    嘤嘤嘤

---
+++ spring_cloud/eureka_8000/src/main/java/com/nico/EurekaWar.java
@@ -0,0 +1,14 @@
+package com.nico;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EurekaWar extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaWar.class);
+    }
+}
