1345438ea46319acf255b71e40ede401541211ca
fangshu <nico@nico.com>
2018-12-28 15:21:15 +0800

    嘤嘤嘤

--- spring_cloud/eureka_8001/src/main/java/com/nico/APP.java
+++ spring_cloud/eureka_8001/src/main/java/com/nico/APP.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class APP {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(APP.class , args);
 	}
